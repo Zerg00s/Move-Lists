379f880b6c66c23d3ca1556c86b1bc29320c2b03
Denis Molodtsov <denis-ne@yandex.ru>
2022-02-17 15:50:29 -0500

    added migration of fields and content types

--- Move-Lists.ps1
+++ Move-Lists.ps1
@@ -27,8 +27,7 @@ $Migration = @{
     Lists       = ""
 }
 
-$Migration = Get-FormItemProperties -item $Migration -dialogTitle "Enter source and target sites" -propertiesOrder @("Source_Site", "Target_Site", "Lists") -note "List titles, comma-separated. Leave blank to have an interactive selector"
-
+$Migration = Get-FormItemProperties -item $Migration -dialogTitle "Enter source and target sites" -propertiesOrder @("Source_Site", "Target_Site", "Lists") -note "List titles are comma-separated. Leave blank to have an interactive selector"
 
 Connect-PnPOnline -Url $Migration.Source_Site -UseWebLogin -WarningAction Ignore
 
@@ -36,21 +35,41 @@ if ($Migration.Lists) {
     $titles = $Migration.Lists.Split(",")
 }
 else {
-    
     $lists = Get-PnPList
-    $lists = $lists | Where-Object {$_.Hidden -eq $false}
+    $lists = $lists | Where-Object { $_.Hidden -eq $false }
     
     $selectedLists = Get-FormArrayItems ($lists) -dialogTitle "Select lists and libraries to migrate" -key Title
     $titles = $selectedLists.Title
+
+    # If a list contains custom content types, we will have to move them all.
+    $exportContentTypes = $false
+    foreach ($list in $selectedLists) {
+        $listContentTypes = Get-PnPContentType -List $list         
+        $context = $list.Context        
+        foreach ($contentType in $listContentTypes) {
+            $context.Load($contentType)
+            $context.ExecuteQuery()  
+            [xml]$xml = $contentType.SchemaXml 
+            if ($null -eq $xml.ContentType.FeatureId) {
+                $exportContentTypes = $true;
+            }
+        }
+    } 
 }
 
-Get-pnpProvisioningTemplate -ListsToExtract $titles -Out "Lists.xml" -Handlers Lists -Force -WarningAction Ignore
-((Get-Content -path Lists.xml -Raw) -replace 'RootSite','Web') | Set-Content -Path Lists.xml
+if ($exportContentTypes) {
+    Get-pnpProvisioningTemplate -ListsToExtract $titles -Out "Lists.xml" -Handlers Lists, ContentTypes, Fields -Force -WarningAction Ignore 
+}
+else {
+    Get-pnpProvisioningTemplate -ListsToExtract $titles -Out "Lists.xml" -Handlers Lists -Force -WarningAction Ignore 
+}
 
-foreach ($title in $titles){
+((Get-Content -path Lists.xml -Raw) -replace 'RootSite', 'Web') | Set-Content -Path Lists.xml
+
+foreach ($title in $titles) {
     # Get the latest list item form layout. Footer, Header and the Body:
     $list = Get-PnPList $title -Includes ContentTypes
-    $contentType = $list.ContentTypes | Where-Object {$_.Name -eq "Item"}
+    $contentType = $list.ContentTypes | Where-Object { $_.Name -eq "Item" }
     $contentType.ClientFormCustomFormatter | Set-Content .\$title.json
 }
 
@@ -59,10 +78,10 @@ Disconnect-PnPOnline
 Connect-PnPOnline -Url $Migration.Target_Site -UseWebLogin
 Apply-PnPProvisioningTemplate -Path Lists.xml 
 
-foreach ($title in $titles){
+foreach ($title in $titles) {
     $list = Get-PnPList $title -Includes ContentTypes
-    $contentType = $list.ContentTypes | Where-Object {$_.Name -eq "Item"}
-    if($contentType){
+    $contentType = $list.ContentTypes | Where-Object { $_.Name -eq "Item" }
+    if ($contentType) {
         $json = Get-Content .\$title.json
         $contentType.ClientFormCustomFormatter = $json
         $contentType.Update($false)
@@ -70,10 +89,8 @@ foreach ($title in $titles){
     }
 }
 
-
 Write-Host "" 
 Write-Host "Target site" $($Migration.Target_Site + "/_layouts/15/viewlsts.aspx") -ForegroundColor Yellow
 
 $Url_To_Open = ($Migration.Target_Site + "/_layouts/15/viewlsts.aspx")
 Start-Process $Url_To_Open
-
